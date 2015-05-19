6b60ad947bfecd933269d09fea6ad39c3dbb2cec
柴源 <769099058@qq.com>
2015-05-19 19:47:15 +0800

    content manager
    添加navigationcontroller

---
+++ CYController/CYNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  CYNavigationController.h
+//  CYHomeManager
+//
+//  Created by chai on 15-5-19.
+//  Copyright (c) 2015年 chai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CYNavigationController : UINavigationController
+
+@end
