78933e8374f6038f918e604d61690327b4eb378d
柴源 <769099058@qq.com>
2015-05-21 23:27:58 +0800

    View edit

---
+++ CYView/CYTextField.m
@@ -0,0 +1,21 @@
+//
+//  CYTextField.m
+//  CYBaseProject
+//
+//  Created by chai on 15-5-21.
+//  Copyright (c) 2015年 chai. All rights reserved.
+//
+
+#import "CYTextField.h"
+
+@implementation CYTextField
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
