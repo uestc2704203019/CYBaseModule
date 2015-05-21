dcd853357242f074cc9aee2b975e5148682d5cc1
柴源 <769099058@qq.com>
2015-05-21 23:28:46 +0800

    view manager

---
+++ CYView/CYImageView.h
@@ -0,0 +1,13 @@
+//
+//  CYImageView.h
+//  CYBaseProject
+//
+//  Created by chai on 15-5-21.
+//  Copyright (c) 2015年 chai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CYImageView : UIImageView
+
+@end
---
+++ CYView/CYImageView.m
@@ -0,0 +1,21 @@
+//
+//  CYImageView.m
+//  CYBaseProject
+//
+//  Created by chai on 15-5-21.
+//  Copyright (c) 2015年 chai. All rights reserved.
+//
+
+#import "CYImageView.h"
+
+@implementation CYImageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
