fe9144ffb2a2e9b2bd9fda08a8b1840d6e79080a
柴源 <769099058@qq.com>
2015-05-25 09:18:23 +0800

    image extension

---
+++ CYModel/CYKit/UIImage+Path.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Path.h
+//  CYBaseProject
+//
+//  Created by chai on 15-5-25.
+//  Copyright (c) 2015年 chai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Path)
+
+@end
---
+++ CYModel/CYKit/UIImage+Path.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+Path.m
+//  CYBaseProject
+//
+//  Created by chai on 15-5-25.
+//  Copyright (c) 2015年 chai. All rights reserved.
+//
+
+#import "UIImage+Path.h"
+
+@implementation UIImage (Path)
+
+@end
