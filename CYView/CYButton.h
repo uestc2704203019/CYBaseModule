e081dfc0b82b9e8959bdb550f8236023a38af697
柴源 <769099058@qq.com>
2015-05-26 21:37:49 +0800

    button 补充

---
+++ CYView/CYButton.h
@@ -0,0 +1,13 @@
+//
+//  CYButton.h
+//  CYBaseProject
+//
+//  Created by chai on 15-5-26.
+//  Copyright (c) 2015年 chai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CYButton : UIButton
+
+@end
---
+++ CYView/CYButton.m
@@ -0,0 +1,21 @@
+//
+//  CYButton.m
+//  CYBaseProject
+//
+//  Created by chai on 15-5-26.
+//  Copyright (c) 2015年 chai. All rights reserved.
+//
+
+#import "CYButton.h"
+
+@implementation CYButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
